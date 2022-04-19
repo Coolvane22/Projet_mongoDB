5eab76c0151ffd14bb7eeae0a4b6d77a0c192682
Coolvane22 <99664579+Coolvane22@users.noreply.github.com>
2022-04-19 15:40:16 +0200

    Requête nombre de créneaux + Moyenne des nombres

--- requetes_python/Visualisation_pymongo.py
+++ requetes_python/Visualisation_pymongo.py
@@ -9,6 +9,7 @@ import pandas as pd
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.tile_providers import get_provider, Vendors
 import numpy as np
+import datetime
 
 
 with open('identifiant.txt') as f:
@@ -17,7 +18,7 @@ with open('identifiant.txt') as f:
 db_uri = id
 client = MongoClient(db_uri)
 
-# Accès bdd doctolib
+# Acces bdd doctolib
 db = client["doctolib"]
 
 #print(db.list_collection_names())
@@ -25,26 +26,25 @@ db = client["doctolib"]
 # On se place dans la collection dump_Jan_2022
 coll = db["dump_Jan2022"]
 
-#Test de connexion 
-query = {"name" : {"$regex": "^R"}}
-cursor = coll.find(query)
-#print(list(cursor))
 
-# Récupération des centres de vaccination situées à moins de 50 km de Rennes:
+# Recuperation des centres de vaccination situees à moins de 50 km de Rennes:
 Rennes  = {'type': 'Point', 'coordinates' :[-1.68002 , 48.111339]}
 # On décide de ne garder que l'id, et les coordonées
 filter_loc = {"location": {'$near': {'$geometry': Rennes, '$maxDistance': 50000}}}
 filter_att = {"name": 1, "location.coordinates" : 1} 
 cursor_centre_50 = coll.find(filter_loc,filter_att)
-#print(list(cursor_centre_50))
+#print(len(list(cursor_centre_50)))
 
-#Création de la liste des noms des centres de vaccinations et des coordonnées
+
+#Creation de la liste des noms des centres de vaccinations et des coordonnees
+
 liste_nom = []
 liste_lon = [] 
 liste_lat = [] 
 
 for rep in cursor_centre_50:
     for k, v in rep.items():
+
         if k == 'name':
             liste_nom.append(v)
      
@@ -58,45 +58,67 @@ dico = {
     'Longitude' : liste_lon,
     'Latitude' : liste_lat
 }
-#Création dun dataset pour l'utilisation des cartes Bokeh
-data = pd.DataFrame(dico)
+
+#Recuperation des creneaux entre le 26 et 29 janvier 2022 inclus 
+filter_geoNear = {'$geoNear': {'near': Rennes,'key' : "location",'distanceField': "distance",'maxDistance': 50000}}
+filter1_unwind = {'$unwind': "$visit_motives"}
+filter2_unwind = {'$unwind': "$visit_motives.slots"}
+filter_match = {'$match': {"visit_motives.slots":{'$gte':datetime.datetime.strptime("2022-01-26", "%Y-%m-%d"),'$lte':datetime.datetime.strptime("2022-01-29", "%Y-%m-%d")}}}
 
 
-# Création de l'output HTML
-output_file("Projet_MongoDB_Python.html")
+cursor_creneaux = coll.aggregate([filter_geoNear, filter1_unwind, filter2_unwind,filter_match , {'$group': {'_id': {' nom' : "$name"}, 'nb': {'$sum':1}}}, ])
 
-#Création de la figure 
-## Nous transformons donc les points GPS:
-k = 6378137
+#On calcule la moyenne pour déterminer le seuil orange/vert
+cursor_moyenne = coll.aggregate([filter_geoNear, filter1_unwind, filter2_unwind,filter_match , {'$group': {'_id': {' nom' : "$name"}, 'nb': {'$sum':1}}},    {'$group': {'_id': { 'nom' : "$name"},'nb': {'$avg':"$nb"}}}, ])
 
-data['Longitude'] = data['Longitude']  * (k * np.pi / 180.0)
-data['Latitude'] = np.log(np.tan((90 + data['Latitude']) * np.pi / 360.0)) * k
+for rep in cursor_moyenne:
+    moyenne = rep['nb']
 
-data_source = ColumnDataSource(data)
+liste_nom_ouvert = []
+for rep in cursor_creneaux:
+     for k, v in rep.items():
+         if k == 'name':
+             liste_nom_ouvert.append(v)
+         if k == ''
+# #Creation dun dataset pour l'utilisation des cartes Bokeh
+# data = pd.DataFrame(dico)
 
-# ## Chargement du fond de carte 
-tile_provider = get_provider(Vendors.CARTODBPOSITRON)
 
-# # Création tools
-tools = "pan,wheel_zoom,box_zoom,reset"
-TOOLTIPS = [
-    ('Nom', '@Nom'),
-    ]
+# # Création de l'output HTML
+# output_file("Projet_MongoDB_Python.html")
 
-# Initialisation figure
-c = figure(
-           x_axis_type="mercator", y_axis_type="mercator",
-           title = "Carte des centres de vaccinations autour de Rennes",
-           tooltips=TOOLTIPS,
-           tools=tools,
-           plot_width=900,
-           plot_height=600
-           )
+# #Creation de la figure 
+# ## Nous transformons donc les points GPS:
+# k = 6378137
 
-# Ajout du fond de carte 
-c.add_tile(tile_provider)
+# data['Longitude'] = data['Longitude']  * (k * np.pi / 180.0)
+# data['Latitude'] = np.log(np.tan((90 + data['Latitude']) * np.pi / 360.0)) * k
 
-#Ajout point 
-c.scatter(x='Longitude', y='Latitude', size=12, alpha=0.5, source=data_source,  
-                )
-show(c)
+# data_source = ColumnDataSource(data)
+
+# # ## Chargement du fond de carte 
+# tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+
+# # # Creation tools
+# tools = "pan,wheel_zoom,box_zoom,reset"
+# TOOLTIPS = [
+#     ('Nom', '@Nom'),
+#     ]
+
+# # Initialisation figure
+# c = figure(
+#            x_axis_type="mercator", y_axis_type="mercator",
+#            title = "Carte des centres de vaccinations autour de Rennes",
+#            tooltips=TOOLTIPS,
+#            tools=tools,
+#            plot_width=900,
+#            plot_height=600
+#            )
+
+# # Ajout du fond de carte 
+# c.add_tile(tile_provider)
+
+# #Ajout point 
+# c.scatter(x='Longitude', y='Latitude', size=12, alpha=0.5, source=data_source,  
+#                 )
+# show(c)
