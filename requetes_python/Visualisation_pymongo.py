4bc6652983a118c7c177c5b0f9df6896b88b5822
Coolvane22 <99664579+Coolvane22@users.noreply.github.com>
2022-04-19 23:40:24 +0200

    Rajout structure tabs

--- requetes_python/Visualisation_pymongo.py
+++ requetes_python/Visualisation_pymongo.py
@@ -9,6 +9,9 @@ from sqlalchemy import null, true
 import pandas as pd
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.tile_providers import get_provider, Vendors
+from bokeh.layouts import row, column
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.models import  Div
 import numpy as np
 import datetime
 import statistics
@@ -76,90 +79,90 @@ cursor_moyenne = coll.aggregate([filter_geoNear, filter1_unwind, filter2_unwind,
 # #L'opérateur médiane n'existe pas dans les opérateurs d'aggregation en mongoDB
 # #On la calcule grâce au package statistics sur python 
 
-# liste_nom_ouvert = []
-# liste_creneau = []
-# for rep in cursor_creneaux:    
-#     for k, v in rep.items():
-#         if k == '_id':
-#             for v1 in v.values():
-#                 liste_nom_ouvert.append(v1)
-#         if k == 'nb':
-#             liste_creneau.append(v)
-# mediane = statistics.median(liste_creneau)
+liste_nom_ouvert = []
+liste_creneau = []
+for rep in cursor_creneaux:    
+    for k, v in rep.items():
+        if k == '_id':
+            for v1 in v.values():
+                liste_nom_ouvert.append(v1)
+        if k == 'nb':
+            liste_creneau.append(v)
+mediane = statistics.median(liste_creneau)
 
 # # # # Creation dun dataset pour l'utilisation des cartes Bokeh
-# # Creation d'un dictionnnaire nom du centre : creneaux disponibles
-# keys_list = liste_nom_ouvert
-# values_list = liste_creneau
-# zip_iterator = zip(keys_list, values_list)
-# dico_creneau = dict(zip_iterator)
+# Creation d'un dictionnnaire nom du centre : creneaux disponibles
+keys_list = liste_nom_ouvert
+values_list = liste_creneau
+zip_iterator = zip(keys_list, values_list)
+dico_creneau = dict(zip_iterator)
 
-# dico_creneau_tot = {}
-# for nom in liste_nom:
-#     if nom in dico_creneau.keys():
-#         dico_creneau_tot[nom] = dico_creneau[nom]
-#     else:
-#         dico_creneau_tot[nom] = 0
+dico_creneau_tot = {}
+for nom in liste_nom:
+    if nom in dico_creneau.keys():
+        dico_creneau_tot[nom] = dico_creneau[nom]
+    else:
+        dico_creneau_tot[nom] = 0
 
-# # Creation d'une liste des couleurs correspondants aux creneaux disposnibles
-# # Rouge si aucun créneau disponible, orange si inférieur à la médiane et vert si supérieur à la médiane
-# color = []
-# for v in dico_creneau_tot.values():
-#     if int(v) == 0:
-#         color.append('red')
-#     elif  int(v) > mediane:
-#         color.append('green')
-#     else:
-#         color.append('orange')
+# Creation d'une liste des couleurs correspondants aux creneaux disposnibles
+# Rouge si aucun créneau disponible, orange si inférieur à la médiane et vert si supérieur à la médiane
+color = []
+for v in dico_creneau_tot.values():
+    if int(v) == 0:
+        color.append('red')
+    elif  int(v) > mediane:
+        color.append('green')
+    else:
+        color.append('orange')
 
 
-# dico = {
-#     'Nom': liste_nom,
-#     'Longitude' : liste_lon,
-#     'Latitude' : liste_lat,
-#     'Creneau' : dico_creneau_tot.values() ,
-#     'Color' : color
-# }
-# data = pd.DataFrame(dico)
+dico = {
+    'Nom': liste_nom,
+    'Longitude' : liste_lon,
+    'Latitude' : liste_lat,
+    'Creneau' : dico_creneau_tot.values() ,
+    'Color' : color
+}
+data = pd.DataFrame(dico)
 
-# # Création de l'output HTML
-# output_file("Projet_MongoDB_Python.html")
+# Création de l'output HTML
+output_file("Projet_MongoDB_Python.html")
 
-# #Creation de la figure 
-# # # Nous transformons donc les points GPS:
-# k = 6378137
+#Creation de la figure 
+# # Nous transformons donc les points GPS:
+k = 6378137
 
-# data['Longitude'] = data['Longitude']  * (k * np.pi / 180.0)
-# data['Latitude'] = np.log(np.tan((90 + data['Latitude']) * np.pi / 360.0)) * k
+data['Longitude'] = data['Longitude']  * (k * np.pi / 180.0)
+data['Latitude'] = np.log(np.tan((90 + data['Latitude']) * np.pi / 360.0)) * k
 
-# data_source = ColumnDataSource(data)
+data_source = ColumnDataSource(data)
 
-# # # # Chargement du fond de carte 
-# tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+# # # Chargement du fond de carte 
+tile_provider = get_provider(Vendors.CARTODBPOSITRON)
 
-# # # Creation tools
-# tools = "pan,wheel_zoom,box_zoom,reset"
-# TOOLTIPS = [
-#     ('Nom', '@Nom'),
-#     ('Créneau','@Creneau')
-#     ]
+# # Creation tools
+tools = "pan,wheel_zoom,box_zoom,reset"
+TOOLTIPS = [
+    ('Nom', '@Nom'),
+    ('Créneau','@Creneau')
+    ]
 
-# # Initialisation figure
-# c = figure(
-#            x_axis_type="mercator", y_axis_type="mercator",
-#            title = "Carte des centres de vaccinations autour de Rennes",
-#            tooltips=TOOLTIPS,
-#            tools=tools,
-#            plot_width=900,
-#            plot_height=600
-#            )
+# Initialisation figure
+c = figure(
+           x_axis_type="mercator", y_axis_type="mercator",
+           title = "Carte des centres de vaccinations autour de Rennes",
+           tooltips=TOOLTIPS,
+           tools=tools,
+           plot_width=900,
+           plot_height=600
+           )
 
-# # Ajout du fond de carte 
-# c.add_tile(tile_provider)
+# Ajout du fond de carte 
+c.add_tile(tile_provider)
 
-# #Ajout des points avec la couleur  adaptee
+#Ajout des points avec la couleur  adaptee
 
-# c.scatter(x='Longitude', y='Latitude', size=12, alpha=0.5, source=data_source,  color = 'Color'        )
+c.scatter(x='Longitude', y='Latitude', size=12, alpha=0.5, source=data_source,  color = 'Color'        )
 
 
 ### 2eme visualisation : Première dose du vaccin 
@@ -174,12 +177,11 @@ liste_nom_ouvert_1 = []
 liste_creneau_1 = []
 for rep in cursor_creneaux_1:    
     for k, v in rep.items():
-        print("Entree :", k)
         if k == '_id':
             for v1 in v.values():
                 liste_nom_ouvert_1.append(v1)
         if k == 'nb':
-            print(k)
+            
             liste_creneau_1.append(v)
 mediane = statistics.median(liste_creneau_1)
 
@@ -259,5 +261,25 @@ s.add_tile(tile_provider)
 s.scatter(x='Longitude', y='Latitude', size=12, alpha=0.5, source=data_source,  color = 'Color'        )
 
 
-show(s)
 
+texte =Div(text="""
+<p> Visualisation PYmongo - Projet Goutard Amélie - Thivend Evane  </p>
+<p> Voici la carte représentant les centres de Vaccination à 50km de Rennes</p>
+<p> Les couleurs des points correspondent à la disponibilité des créneaux entre le 26 janvier 2022 et le 29 janvier 2022</p>
+""")
+
+layout = column(texte, c)
+
+texte_1 =Div(text="""
+<p> Visualisation PYmongo - Projet Goutard Amélie - Thivend Evane  </p>
+<p> Voici la carte représentant les centres de Vaccination à 50km de Rennes</p>
+<p> Les couleurs des points correspondent à la disponibilité des créneaux réservés à la première dose du vaccin entre le 1er janvier 2022 et le 1er juin 2022</p>
+""")
+layout_1 = column(texte_1, s)
+
+tab1 = Panel(child=layout, title="Visualisation des créneaux du 26 au 29 janvier 2022")
+tab2 = Panel(child=layout_1, title="Visualisation des créneaux réservé à la première dose du 1er janvier au 1er juin  2022")
+tabs = Tabs(tabs = [tab1, tab2])
+
+
+show(tabs)
