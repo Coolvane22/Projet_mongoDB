b2204ec54f929d08ed15b6b727e01b6b858d1abf
amelie-goutard <80648016+ameliegtrd@users.noreply.github.com>
2022-04-26 00:27:37 +0200

    Add files via upload

--- requetes_R/ui.R
+++ requetes_R/ui.R
@@ -7,16 +7,34 @@
 
 #### CONTENU DES PAGES
 publication <- fluidRow(
-  box(
-    title ="Publications", 
-    status = "info", 
-    solidHeader = TRUE, 
-    width = 12,
-    collapsible = TRUE, 
-    align="justify",
-    "Bla bla",
-    visNetworkOutput("reseaux")
-  )
+    box(
+      title =tagList(icon("book-reader"), "  Publication"), 
+      status = "info", 
+      solidHeader = TRUE, 
+      width = 12,
+      collapsible = TRUE, 
+      align="justify",
+      "La base de données 'publications' contient les informations relatives 
+    aux publications de scientifiques du laboratoire IRISA pour l'année 2021.", br(),
+      "Le graphique ci-dessous permet de visualiser les liens entre les auteurs
+    de ces publications. Nous avons uniquement gardé les 20 auteurs qui ont 
+    participé à l'écriture du plus grand nombre d'articles.", br(),
+      "Vous pouvez choisir de visualiser plus particulièrement un groupe d'auteurs.
+    En effet, nous avons regroupé les auteurs en 3 catégories, selon leur nombres de
+    publications. Les auteurs ayant un nombre de publication supérieur à 12 sont 
+    considérés commes 'High'. Ceux qui ont participés à 11 ou 12 publications comme 
+    'Medium' et enfin ceux qui ont un nombre de publications inférieur à 11 comme 'Low' ", 
+      br(), "Aussi, l'épaisseur des traits varie en fonction du nombre de publication en commun
+    entre auteurs."
+    ),
+    box(
+      title = tagList(icon("project-diagram"), "  Représentation des liens"), 
+      status = "info", 
+      solidHeader = TRUE,
+      collapsible = TRUE,
+      width = 12,
+      visNetworkOutput("reseaux")
+    )
 )
   
 food <- fluidRow(
@@ -29,21 +47,25 @@ food <- fluidRow(
     align="justify",
     "Bla bla"
   ),
-  sidebarPanel(
-    selectizeInput(
-      'choix_type_cuisine', 
-      'Choix du type de cuisine :',
-      choices = unique(food$id$cuisine)
-    )
-  ),
-  tabsetPanel(
+  tabBox(
+    width = 12,
+    title = "Graphiques",
+    status = "purple",
+    collapsible = TRUE,
+    id = "tabset1",
+    side = "right",
     tabPanel(
-      "Nombres de restaus de ce type dans chaque quartier", 
+      tagList(icon("chart-pie"), "  Graph 1"),
+      selectizeInput(
+        'choix_type_cuisine', 
+        'Choix du type de cuisine :',
+        choices = unique(food$id$cuisine)
+      ),
       plotlyOutput("plot_type")
     ),
     tabPanel(
-      "2eme graphe", 
-      plotlyOutput("plot_2")
+      tagList(icon("chart-pie"), "  Graph 2"),
+      "bla bla"
     )
   )
 )
@@ -68,7 +90,7 @@ ui <- dashboardPage(
         "Base publications", tabName = "publi", icon = icon("book", lib="font-awesome")
       ),
       menuItem(
-        "Base NYfood",tabName = "nyfood", icon = icon("cutlery", lib="font-awesome")
+        "Base NYfood",tabName = "nyfood", icon = icon("fas fa-utensils", lib="font-awesome")
       )
     )
   ),
